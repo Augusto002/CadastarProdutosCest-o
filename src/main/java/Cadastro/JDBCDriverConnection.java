e2eae988f8fdc644d378e3840b66a4a2532698c5
Augusto002 <henrique.augustob27@gmail.com>
2021-07-24 13:52:50 -0300

    Conexao com SQL Server Express

--- src/main/java/Cadastro/JDBCDriverConnection.java
+++ src/main/java/Cadastro/JDBCDriverConnection.java
@@ -1,58 +1,33 @@
 package Cadastro;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class JDBCDriverConnection {
 
-    public static Connection connect() {
-        Connection conn = null;
+    public Connection getConnection(){
         try {
-            // db parameters
-            String url = "jdbc:sqlite:C:/Users/henri/Desktop/Estudos/CadastarProdutosCestão/Teste";
-            // create a connection to the database
-            conn = DriverManager.getConnection(url);
+            return  DriverManager.getConnection(
+                    "jdbc:sqlserver://Server=localhost\\SQLEXPRESS;Database=Teste;Trusted_Connection=True;"
+            );
 
-            System.out.println("Connection to SQLite has been established.");
 
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
-        return conn;
-    }
-
-
-
-   public void insert(String name, int unidade,String data) {
-        String sql = "INSERT INTO Produto(Nome,Unidade,Data) VALUES ('')";
-
-        try (Connection conn = this.connect();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, name);
-            pstmt.setDouble(2, unidade);
-            pstmt.setString(3,data);
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+           throw new RuntimeException(e);
         }
     }
+
+
+
+
+
 
 
 
         public static void main(String[]args){
-      JDBCDriverConnection cx=new JDBCDriverConnection();
-      connect();
-   cx.insert("testpro",3,"21/07/2021");
-
+      JDBCDriverConnection cx = new JDBCDriverConnection();
+      cx.getConnection();
         }
     }
 
